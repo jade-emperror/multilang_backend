b5e71dfe7dbc6e06abafdefd4a595f261c0ce8b9
jade-emperror <maestrobharath@gmail.com>
2022-01-14 17:38:09 +0530

    performed generalization of response variable

--- api/views.py
+++ api/views.py
@@ -29,8 +29,23 @@ class getdata(APIView):
         lang_code = request.GET.get('lang_code')
         if(lang_code in allowed_langs):
             if(lang_code=='ta'):
-                return JsonResponse(list(Product.objects.values('prod_id','prod_name_ta','prod_desc_ta','prod_class_ta')),safe=False)
+                data=list(Product.objects.values('prod_id','prod_name_ta','prod_desc_ta','prod_class_ta'))
+                for i in data:
+                    i['prod_name']=i.pop('prod_name_ta')
+                    i['prod_class']=i.pop('prod_class_ta')
+                    i['prod_desc']=i.pop('prod_desc_ta')
+                return JsonResponse(data,safe=False)
             elif (lang_code == 'ka'):
-                return JsonResponse(list(Product.objects.values('prod_id','prod_name_ka','prod_desc_ka','prod_class_ka')),safe=False)
+                data=list(Product.objects.values('prod_id','prod_name_ka','prod_desc_ka','prod_class_ka'))
+                for i in data:
+                    i['prod_name']=i.pop('prod_name_ka')
+                    i['prod_class']=i.pop('prod_class_ka')
+                    i['prod_desc']=i.pop('prod_desc_ka')
+                return JsonResponse(data,safe=False)
         else:
-            return JsonResponse(list(Product.objects.values('prod_id','prod_name_en','prod_desc_en','prod_class_en')),safe=False)
+            data=list(Product.objects.values('prod_id','prod_name_en','prod_desc_en','prod_class_en'))
+            for i in data:
+                    i['prod_name']=i.pop('prod_name_en')
+                    i['prod_class']=i.pop('prod_class_en')
+                    i['prod_desc']=i.pop('prod_desc_en')
+            return JsonResponse(data,safe=False)
